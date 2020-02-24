025f077d9481260f78a1bedf1dc43c2685bd07c4
Jwillsher <jak.willsher@hotmail.co.uk>
2020-02-24 12:10:03 +0100

    react time

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+
+
+// this imports css files
 import logo from './logo.svg';
 import './App.css';
+// this works as require, require is an old way. its the same as require. 
+// this move things within files. 
 
-function App() {
-  return (
+
+
+
+function App() { // componate which allows html. // this will show up on the page. 
+  return ( // must put somwthing on this line. 
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Today i <strong> walked  </strong>. 
         </p>
         <a
           className="App-link"
@@ -16,11 +24,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello there! :)
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default App; /*Js file exports a file. App is a function which gets exported. 
+this is a function call in react. importing file in antoehr file, then creates new file. 
+*/
